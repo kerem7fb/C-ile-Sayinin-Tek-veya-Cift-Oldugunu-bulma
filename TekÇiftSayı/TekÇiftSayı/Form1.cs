4acc794d8e0293e68fb73c0df0a82a136737c034
kerem7fb <117513389+kerem7fb@users.noreply.github.com>
2024-11-03 14:10:25 +0300

    Add files via upload

---
+++ TekÇiftSayı/TekÇiftSayı/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TekÇiftSayı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi, islem;
+            sayi = Convert.ToInt32(textBox1.Text);
+            islem = sayi % 2;
+            if (islem == 0) 
+            {
+                MessageBox.Show(sayi + " Sayısı Çifttir");
+            }
+            if (islem == 1) 
+            {
+                MessageBox.Show(sayi + " Sayısı Tektir");
+            }
+        }
+    }
+}
